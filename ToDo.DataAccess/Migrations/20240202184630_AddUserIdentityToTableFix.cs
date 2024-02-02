0d873add2a9ba49c92344865f3aa2901ec33a4a7
alumpish <apadyav@gmail.com>
2024-02-02 22:37:25 +0330

    Add Identity

---
+++ ToDo.DataAccess/Migrations/20240202184630_AddUserIdentityToTableFix.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdentityToTableFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,8 +1,15 @@
+using BulkyBook.DataAcess.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
